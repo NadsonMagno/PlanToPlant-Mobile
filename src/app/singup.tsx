8b43470944da84abd4106f160f19dbf1e7989f29
tanatoyt <tanatoyt@gmail.com>
2025-04-17 22:46:50 +0000

    sinc feature

---
+++ src/app/singup.tsx
@@ -0,0 +1,2 @@
+export { default } from '@/components/screens/SingUp/SingUp'
+
---
+++ src/components/screens/SingUp/SingUp.tsx
@@ -0,0 +1,39 @@
+import React from 'react';
+import {View, Text, TextInput, TouchableOpacity, KeyboardAvoidingView} from 'react-native';
+
+import { styles } from './style';
+import { router } from 'expo-router';
+
+export default function SingUp() {
+
+  function handleLogin(){
+    
+    router.navigate('/index')
+
+  }
+
+
+
+  return (
+    <KeyboardAvoidingView style={styles.fundo}>
+    
+    <View style={styles.container}>
+       
+    <TextInput placeholder="UserName" autoCorrect={false} onChange={()=>{}} style={styles.input}/>
+
+        <TextInput placeholder="Email" autoCorrect={false} onChange={()=>{}} style={styles.input}/>
+          
+        <TextInput placeholder="Senha" autoCorrect={false} onChange={()=>{}} style={styles.input}/>
+      
+        <TouchableOpacity style={styles.btnSubmit} onPress={handleLogin}>
+            <Text>Criar conta</Text>
+        </TouchableOpacity>
+
+        <TouchableOpacity style={styles.btnRegister}>
+            <Text style={styles.registerText}>Login</Text>
+
+        </TouchableOpacity>
+    </View>
+    </KeyboardAvoidingView>     
+  );
+}
